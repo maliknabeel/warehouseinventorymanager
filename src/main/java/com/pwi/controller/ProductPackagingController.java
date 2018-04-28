cec55f6cb2334dab0b9f34a478886f0aaa791ecb
Malik Nabeel Ahmed Awan <nabeel.ahmed@seecs.edu.pk>
2018-04-28 23:07:38 +0500

    All product packaging services

---
+++ src/main/java/com/pwi/controller/ProductPackagingController.java
@@ -0,0 +1,41 @@
+package com.pwi.controller;
+
+import com.pwi.entities.ProductPkgingEntity;
+import com.pwi.services.ProductPkgingServices;
+import com.pwi.utils.ResponseBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/packaging")
+public class ProductPackagingController {
+
+    @Autowired
+    ProductPkgingServices productPkgingServices;
+
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<Object> getAllPackagingDetails(){
+        return new ResponseEntity<>(productPkgingServices.getProductsPkgingList(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Object> addPackagingDetails(@RequestBody ProductPkgingEntity productPkgingEntity){
+        return new ResponseEntity<>(productPkgingServices.createOrUpdateProductsPkging(productPkgingEntity), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Object> upadtePackagingDetails(@RequestBody ProductPkgingEntity productPkgingEntity){
+        return new ResponseEntity<>(productPkgingServices.createOrUpdateProductsPkging(productPkgingEntity), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Object>deletePackagingDetails(@RequestBody ProductPkgingEntity productPkgingEntity){
+        productPkgingServices.deleteProductsPkging(productPkgingEntity);
+        return new ResponseEntity<>(new ResponseBody(true, "Deletion Successful"), HttpStatus.OK);
+    }
+}
