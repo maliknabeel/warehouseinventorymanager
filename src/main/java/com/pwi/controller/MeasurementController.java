ad979ad951d1da7097ddeaf43efaca99d3e5d79d
Malik Nabeel Ahmed Awan <nabeel.ahmed@seecs.edu.pk>
2018-04-28 23:02:36 +0500

    All measurement services

--- src/main/java/com/pwi/controller/MeasurementController.java
+++ src/main/java/com/pwi/controller/MeasurementController.java
@@ -1,12 +1,15 @@
 package com.pwi.controller;
 
+import com.pwi.entities.MeasurementEntity;
 import com.pwi.services.MeasurementsServices;
+import com.pwi.utils.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -24,4 +27,19 @@ public class MeasurementController {
         return new ResponseEntity<>(measurementsServices.getPrdMeasurmentList(), HttpStatus.OK);
     }
 
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Object> addMeasurements(@RequestBody MeasurementEntity measurementEntity){
+        return new ResponseEntity<>(measurementsServices.createOrUpdatePrdMeasurement(measurementEntity), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateMeasurements(@RequestBody MeasurementEntity measurementEntity){
+        return new ResponseEntity<>(measurementsServices.createOrUpdatePrdMeasurement(measurementEntity), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteMeasurements(@RequestBody MeasurementEntity measurementEntity){
+        measurementsServices.deletePrdMeasurement(measurementEntity);
+        return new ResponseEntity<>(new ResponseBody(true, "Successfully Deleted"), HttpStatus.OK);
+    }
 }
