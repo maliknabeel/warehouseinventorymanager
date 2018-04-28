b936f85dce6b820f684fcebbca2eb1be1c8f2b4a
Malik Nabeel Ahmed Awan <nabeel.ahmed@seecs.edu.pk>
2018-04-28 22:47:53 +0500

    All inventory services

--- src/main/java/com/pwi/controller/InventoryController.java
+++ src/main/java/com/pwi/controller/InventoryController.java
@@ -1,7 +1,9 @@
 package com.pwi.controller;
 
+import com.pwi.entities.InventoryEntity;
 import com.pwi.services.InventoryCustomReportService;
 import com.pwi.services.InventoryServices;
+import com.pwi.utils.ResponseBody;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -25,5 +28,21 @@ public class InventoryController {
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<Object> getCompleteInventoryList(){
         return new ResponseEntity<>(inventoryServices.getInventoryList(), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Object> addInventory(@RequestBody InventoryEntity inventoryEntity){
+        return new ResponseEntity<>(inventoryServices.createOrUpdateInventory(inventoryEntity), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT)
+    public ResponseEntity<Object> updateInventory(@RequestBody InventoryEntity inventoryEntity){
+        return new ResponseEntity<>(inventoryServices.createOrUpdateInventory(inventoryEntity), HttpStatus.OK);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    public ResponseEntity<Object> deleteInventory(@RequestBody InventoryEntity inventoryEntity){
+        inventoryServices.deleteInventory(inventoryEntity);
+        return new ResponseEntity<>(new ResponseBody(true, "Inventory deleted successfullt"), HttpStatus.OK);
     }
 }
